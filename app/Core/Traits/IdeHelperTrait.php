fd96b7933d14ce777f8352a9ae5015c81ae52b4f
张志辉 <mr.zhang@zhangzhihuideMacBook-Pro.local>
2020-04-09 11:20:58 +0800

    fix

--- app/Core/Traits/IdeHelperTrait.php
+++ app/Core/Traits/IdeHelperTrait.php
@@ -13,6 +13,8 @@ use Hyperf\Database\Model\Builder;
  * @method static $this|\Hyperf\Database\Model\Model create(array $attributes = [])
  * @method static $this|\Hyperf\Database\Model\Model forceCreate(array $attributes)
  * @method static \Hyperf\Database\Model\Model|static findOrNew($id, $columns = ['*'])
+ * @method static \Hyperf\Database\Query\Builder where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method static \Hyperf\Database\Query\Builder whereIn($column, $values, $boolean = 'and', $not = false)
  * @method static \Hyperf\Database\Model\Collection|\Hyperf\Database\Model\Model|static|static[] findOrFail($id, $columns = ['*'])
  * @method static \Hyperf\Database\Model\Collection|static[] findMany($ids, $columns = ['*'])
  * @method static \Hyperf\Database\Model\Model|static firstOrNew(array $attributes, array $values = [])
